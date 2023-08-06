4e5e3da6824b52c771260e71c0e8f06d206597ab
Kodex <gcavedal@gmail.com>
2023-08-05 22:57:02 -0300

    clean and working

--- tel-bot.py
+++ tel-bot.py
@@ -1,4 +1,6 @@
 import os, io
+import asyncio
+import httpx
 from dotenv import load_dotenv
 from loguru import logger
 from telegram import ForceReply, Update
@@ -7,34 +9,34 @@ import whisper
 
 load_dotenv()
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
-
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
-
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    await update.message.reply_text(update.message.text)
+    logger.debug(f"Echo message: '{update.message.text}' received from user: {update.effective_user.id} in chat: {update.effective_chat.id}")
 
 
 async def audio_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Handle received audio."""
+    logger.debug(f"Audio message received from user: {update.effective_user.id} in chat: {update.effective_chat.id}")
+
     audio_file = update.message.audio or update.message.voice  # Get audio from the message
 
     if audio_file:
-        file = await context.bot.get_file(audio_file.file_id)
+        retry = 0
+        max_retry = 5
+        while retry < max_retry:
+            try:
+                file = await context.bot.get_file(audio_file.file_id)
+                break
+            except httpx.ReadTimeout:
+                retry += 1
+                await asyncio.sleep(5)  # wait for 5 seconds before retrying
+        else:  # if it still fails after max_retry attempts, give up
+            logger.error("Failed to download file after multiple attempts")
+            return
+
         filepath = os.path.join("audios", f"{audio_file.file_id}.ogg")
-        await file.download_to_drive(filepath)
-        logger.debug(f"file {filepath} downloaded")
+        await file.download_to_drive(filepath)  # Using download method here instead of download_to_drive
+        logger.debug(f"File {filepath} downloaded")
 
         model = whisper.load_model("base")
 
@@ -53,19 +55,10 @@ async def audio_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token(os.getenv("TOKEN")).build()
 
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # handle audio
     application.add_handler(MessageHandler(filters.AUDIO | filters.VOICE, audio_handler))
-
-    # on non command i.e message - echo the message on Telegram
-    # application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.AUDIO & ~filters.VOICE, audio_handler))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
